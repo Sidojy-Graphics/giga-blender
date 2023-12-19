0c79f50f68630ba55b0f6d4b7175fef203e9a53b
Sergey Kulikov <ksa@raa-st.com>
2023-12-19 10:37:58 +0300

    Create init file

---
+++ __init__.py
@@ -0,0 +1,18 @@
+bl_info = {
+    "name": "GigaChat integration for blender",
+    "author": "Sergey Kulikov",
+    "blender": (3, 3, 0),
+    "version": (0, 0, 1),
+    "location": "3D View -> Tool Shelf -> GigaChat",
+    "category": "Utils"
+}
+
+
+def register():
+    pass
+
+def unregister():
+    pass
+
+if __name__ == "__main__":
+    register()
